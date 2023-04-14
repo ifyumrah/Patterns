c1d5edc37fc5e54741fcecefc8466c98f5617c4d
ify <oge_umerah25@milton.edu>
2023-04-13 22:09:04 -0400

    work on pattern 6 (unsuccessful)

---
+++ src/PatternMarker.java
@@ -0,0 +1,49 @@
+public class PatternMarker {
+    public static void main(String[] args) {
+        PatternMarker stripes = new PatternMarker();
+    }
+
+    public PatternMarker() {
+        System.out.println("Hi pattern maker");
+//        pattern1();
+        pattern5();
+    }
+
+    public void pattern1() {
+        for (int x = 0; x < 6; x++) {
+//            for (int y = 0; y < 5; y++) {
+            System.out.println(x * 5);
+            //}
+        }
+    }
+
+    public void pattern2() {
+        for (int x = 0; x < 6; x++) {
+            System.out.println(2 + x * 5);
+        }
+    }
+
+    public void pattern3() {
+        for (int x = 21; x >0; x=x-4) {
+            System.out.print(x +"\t");
+        }
+    }
+    public void pattern4() {
+        for (int w = 0; w<4; w++) {
+            System.out.println();
+            for (int c = 1; c <5; c++) {
+            System.out.print(c + "\t");
+        }
+
+}
+}
+    public void pattern5() {
+        for (int w = 0; w<5; w++) {
+            for (int c = 1; c<=w; c++) {
+                System.out.print(w);
+            }
+                System.out.println();
+            }
+
+        }
+    }
